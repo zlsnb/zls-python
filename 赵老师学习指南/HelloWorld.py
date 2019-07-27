e6fdd91653fcb306512c9b3a403d7d21d82492d3
fuhailong <fuhailong@meituan.com>
2019-07-27 23:40:58 +0800

    add hello world

---
+++ 赵老师学习指南/HelloWorld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+
+print("Hello World")
+print("你好，世界")
+print "Hello world"
+
+# 学习的第一个程序就是HelloWorld
+# 本文的版本使用的是python3
+# 代码的前两行即为固定的格式，表示的是python的位置以及相应的编码
+# 使用print进行打印，前两行为python3的打印方式，这个类似于c++等语言的打印方式
+# 最后一行为python2 的打印方式，我只是做个例子，以后的print尽量使用python3的格式
