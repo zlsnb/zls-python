4789543e248e1c08d6ef30fb24b15ca59d92350e
fuhailong <fuhailong@meituan.com>
2019-08-08 14:57:09 +0800

    帮助zls改文档

---
+++ 赵老师学习指南/untitled.java
@@ -0,0 +1,45 @@
+    private Integer id;
+
+    private Integer userId;
+
+    private String provinceCode;
+
+    private String provinceName;
+
+    private String cityCode;
+
+    private String cityName;
+
+    private String regionCode;
+
+    private String regionName;
+
+    private String detailAddress;
+
+    private String postCode;
+
+    private String consignee;
+
+    private String mobilePhone;
+
+    private Integer isDefault;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private String createUser;
+
+    private Date updateTime;
+
+    private String updateUser;
+
+    private String countryCode;
+
+    private String countryName;
+
+    private String floorNum;
+    
+    private String siteCode;
+
+    private String roomNum;
